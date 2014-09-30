e67add745a316f76f8e27911392a5e1d3ef59c11
James Badger <james@jamesbadger.ca>
2014-09-30 17:52:29 -0600

    Add WIP demo for switching control

---
+++ examples/layers-switcher/demo.js
@@ -0,0 +1,60 @@
+var map;
+
+var projections = [
+  "EPSG:3571",
+  "EPSG:3572",
+  "EPSG:3573",
+  "EPSG:3574",
+  "EPSG:3575",
+  "EPSG:3576"
+  ];
+
+// Globally define projections for Proj4js. If not defined here, then they must
+// be defined in tile provider definitions below.
+proj4.defs([
+  ["EPSG:3571","+proj=laea +lat_0=90 +lon_0=180 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3572","+proj=laea +lat_0=90 +lon_0=-150 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3573","+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3574","+proj=laea +lat_0=90 +lon_0=-40 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3575","+proj=laea +lat_0=90 +lon_0=10 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3576","+proj=laea +lat_0=90 +lon_0=90 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"]
+]);
+
+// Create object to define tile provider settings and transformations. Supports
+// all Leaflet TileLayer options.
+var projectedTiles = {};
+
+$.each(projections, function (index, value) {
+  var url = "http://tiles.arcticconnect.org/osm_" + (3571 + index) + "/{z}/{x}/{y}.png";
+  projectedTiles["arctic_connect@" + value] = L.PolarMap.tileLayer(url, {
+    name: "arctic_connect@" + value,
+    crs: value,
+    minZoom: 0,
+    maxZoom: 18,
+    tms: false,
+    origin: [-20036842.762, 20036842.762],
+    maxResolution: ((20036842.762 - -20036842.762) / 256),
+    center: [90,0],
+    zoom: 4,
+    continuousWorld: true,
+    attribution: 'Map &copy; <a href="http://arcticconnect.org">ArcticConnect</a>. Data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  });
+});
+
+// Initialization
+$(document).ready(function() {
+  Autosize.enable();
+
+  // Load PolarMap
+  map = L.PolarMap.map('xmap', {
+    baseLayer: projectedTiles["arctic_connect@EPSG:3573"]
+  });
+
+  // Add a Leaflet layer group. Assumed to be EPSG:3857.
+  L.layerGroup([
+    L.marker([51.080126, -114.13380900]).bindPopup("University of Calgary"),
+    L.marker([90, 100]).bindPopup("North Pole")
+  ]).addTo(map);
+
+  L.control.layers(projectedTiles).addTo(map);
+});
