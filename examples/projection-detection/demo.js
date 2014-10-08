1eeb1ac7d52247660c86a6f806196508c0ed6469
James Badger <james@jamesbadger.ca>
2014-10-08 13:53:03 -0600

    Add demo to detect, draw, and zoom to geolocation
    Projection issue with coordinates? They seem to be off.

---
+++ examples/projection-detection/demo.js
@@ -0,0 +1,118 @@
+var map;
+
+var projections = [
+  "EPSG:3571",
+  "EPSG:3572",
+  "EPSG:3573",
+  "EPSG:3574",
+  "EPSG:3575",
+  "EPSG:3576"
+  ];
+
+// Globally define projections for Proj4js. If not defined here, then they must
+// be defined in tile provider definitions below.
+proj4.defs([
+  ["EPSG:3571","+proj=laea +lat_0=90 +lon_0=180 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3572","+proj=laea +lat_0=90 +lon_0=-150 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3573","+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3574","+proj=laea +lat_0=90 +lon_0=-40 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3575","+proj=laea +lat_0=90 +lon_0=10 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"],
+  ["EPSG:3576","+proj=laea +lat_0=90 +lon_0=90 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs"]
+]);
+
+// Create object to define tile provider settings and transformations. Supports
+// all Leaflet TileLayer options.
+var projectedTiles = {};
+
+$.each(projections, function (index, value) {
+  var url = "http://{s}.tiles.arcticconnect.org/osm_" + (3571 + index) + "/{z}/{x}/{y}.png";
+  projectedTiles["Arctic Connect: " + value] = L.PolarMap.tileLayer(url, {
+    name: "Arctic Connect: " + value,
+    crs: value,
+    minZoom: 0,
+    maxZoom: 18,
+    tms: false,
+    origin: [-20036842.762, 20036842.762],
+    maxResolution: ((20036842.762 - -20036842.762) / 256),
+    center: [90,0],
+    zoom: 2,
+    continuousWorld: true,
+    attribution: 'Map &copy; <a href="http://arcticconnect.org">ArcticConnect</a>. Data &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  });
+});
+
+// Set up next/prev linked list
+$.each(projectedTiles, function (layerName, layer) {
+  var keys = Object.keys(projectedTiles);
+  var index = keys.indexOf(layerName);
+  var prev = (index === 0) ? keys.length - 1 : index - 1;
+  var next = (index === keys.length - 1) ? 0 : index + 1;
+
+  layer.prev = projectedTiles[keys[prev]];
+  layer.next = projectedTiles[keys[next]];
+});
+
+// Initialization
+$(document).ready(function() {
+  Autosize.enable();
+
+  // Load PolarMap
+  map = L.PolarMap.map('xmap', {
+    baseLayer: projectedTiles["Arctic Connect: EPSG:3571"]
+  });
+
+  // Add a Leaflet layer group. Assumed to be EPSG:3857.
+  L.layerGroup([
+    L.marker([51.080126, -114.13380900]).bindPopup("University of Calgary"),
+    L.marker([90, 100]).bindPopup("North Pole")
+  ]).addTo(map);
+
+  var layersControl = L.control.layers(projectedTiles, null, {
+    collapsed: false
+  });
+  layersControl.addTo(map);
+
+  // Wire up rotation controls
+  var getBaseLayer = function() {
+    var foundLayer = null;
+    $.each(projectedTiles, function (layerName, layer) {
+      if (map.hasLayer(layer)) {
+        foundLayer = layer;
+      }
+    });
+    return foundLayer;
+  };
+
+  var rotationControls = L.PolarMap.Control.rotation({
+    onRotateCW: function() {
+      map.loadTileProjection(getBaseLayer().next);
+    },
+
+    onRotateCCW: function() {
+      map.loadTileProjection(getBaseLayer().prev);
+    }
+  });
+  rotationControls.addTo(map);
+
+  // Automatic Location and Projection Detection
+  var userLocation = L.circle();
+
+  map.on('locationfound', function (e) {
+    console.log("Location info", e);
+    userLocation.setLatLng(e.latlng);
+    userLocation.setRadius(e.accuracy);
+
+    if (!map.hasLayer(userLocation)) {
+      userLocation.addTo(map);
+    }
+    map.setView(e.latlng, 17);
+  });
+
+  map.on('locationerror', function (e) {
+    console.warn("Location detection error:", e);
+  });
+
+
+  map.locate();
+});
+
